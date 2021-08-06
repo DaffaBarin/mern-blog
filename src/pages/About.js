ebab7d79498ccf94df301c7432572af53da1f539
DaffaBarin <daffabarin@gmail.com>
2021-08-06 10:11:01 +0700

    front end

---
+++ src/pages/About.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="mb-20">
+        <h1 className='sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900'>About</h1>
+        <p className='mx-auto leading-relaxed text-base mb-4'>Nisi non enim qui nulla amet. Non eiusmod exercitation est duis proident.
+            Aute cillum officia voluptate aliquip cupidatat id sint.
+            Deserunt esse commodo nulla veniam anim sint consequat fugiat magna dolor laborum adipisicing sint incididunt.
+            Labore eu et ipsum ut.
+            Reprehenderit eu dolor ea esse cupidatat Lorem.</p>
+        <p className='mx-auto leading-relaxed text-base mb-4'>Nisi non enim qui nulla amet. Non eiusmod exercitation est duis proident.
+            Aute cillum officia voluptate aliquip cupidatat id sint.
+            Deserunt esse commodo nulla veniam anim sint consequat fugiat magna dolor laborum adipisicing sint incididunt.
+            Labore eu et ipsum ut.
+            Reprehenderit eu dolor ea esse cupidatat Lorem.</p>
+        <p className='mx-auto leading-relaxed text-base mb-4'>Nisi non enim qui nulla amet. Non eiusmod exercitation est duis proident.
+            Aute cillum officia voluptate aliquip cupidatat id sint.
+            Deserunt esse commodo nulla veniam anim sint consequat fugiat magna dolor laborum adipisicing sint incididunt.
+            Labore eu et ipsum ut.
+            Reprehenderit eu dolor ea esse cupidatat Lorem.</p>
+        </div>
+    )
+}
+
+export default About
---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className="mb-20">
+        <h1 className='sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900'>Hello, welcome to my go-blog!</h1>
+        <p className='mx-auto leading-relaxed text-base mb-4'>Nisi non enim qui nulla amet. Non eiusmod exercitation est duis proident.
+            Aute cillum officia voluptate aliquip cupidatat id sint.
+            Deserunt esse commodo nulla veniam anim sint consequat fugiat magna dolor laborum adipisicing sint incididunt.
+            Labore eu et ipsum ut.
+            Reprehenderit eu dolor ea esse cupidatat Lorem.</p>
+        <p className='mx-auto leading-relaxed text-base mb-4'>Nisi non enim qui nulla amet. Non eiusmod exercitation est duis proident.
+            Aute cillum officia voluptate aliquip cupidatat id sint.
+            Deserunt esse commodo nulla veniam anim sint consequat fugiat magna dolor laborum adipisicing sint incididunt.
+            Labore eu et ipsum ut.
+            Reprehenderit eu dolor ea esse cupidatat Lorem.</p>
+        <p className='mx-auto leading-relaxed text-base mb-4'>Nisi non enim qui nulla amet. Non eiusmod exercitation est duis proident.
+            Aute cillum officia voluptate aliquip cupidatat id sint.
+            Deseruaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaant esse commodo nulla veniam anim sint consequat fugiat magna dolor laborum adipisicing sint incididunt.
+            Labore eu et ipsum ut.
+            Reprehenderit eu dolor ea esse cupidatat Lorem.</p>
+        </div>
+    )
+}
+
+export default Home
